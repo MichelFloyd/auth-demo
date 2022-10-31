3ebba5a4b0e7ea414f93094918d363a91499a616
MichelFloyd <michel.floyd@cloaklabs.com>
2022-10-31 16:37:01 -0700

    tweaks

--- server/src/jwt.ts
+++ server/src/jwt.ts
@@ -1,37 +1,45 @@
 // function to set jwts
 // see https://www.richardkotze.com/coding/json-web-tokens-using-apollo-graphql
 
+import { compare } from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const { sign, verify } = jwt;
 
-export const setTokens = ({ id }) => {
-  const user = { id };
-  const accessToken = sign({ user }, process.env.ACCESSTOKENSECRET, {
+export const setTokens = (user) => {
+  const accessToken = sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: process.env.ACCESS_TOKEN_DURATION,
   });
-  const refreshToken = sign({ user }, process.env.REFRESHTOKENSECRET, {
+  const refreshToken = sign({ user }, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: process.env.REFRESH_TOKEN_DURATION,
   });
   const expiresAt = new Date();
   expiresAt.setDate(expiresAt.getDate() + 60);
-  return { id, accessToken, refreshToken };
+  return { id: user.id, accessToken, refreshToken };
 };
 
-export const validateAccessToken = (token) => {
+// these two functions wrap verify() in a try/catch to absorb expired jwt errors
+export const validateAccessToken = (token: string) => {
   try {
-    return verify(token, process.env.ACCESSTOKENSECRET);
+    return verify(token, process.env.ACCESS_TOKEN_SECRET);
   } catch (error) {
     if (error.message !== 'jwt expired') console.error(error.message);
-    return null;
   }
 };
 
-export const validateRefreshToken = (token) => {
+export const validateRefreshToken = (token: string) => {
   try {
-    return verify(token, process.env.REFRESHTOKENSECRET);
+    return verify(token, process.env.REFRESH_TOKEN_SECRET);
   } catch (error) {
     if (error.message !== 'jwt expired') console.error(error.message);
-    return null;
   }
+};
+
+export const comparePromise = async (password: string, hash: string) => {
+  return new Promise((resolve, reject) => {
+    compare(password, hash, (err, result) => {
+      if (err) reject(err);
+      else resolve(result);
+    });
+  });
 };
--- server/src/login.ts
+++ server/src/login.ts
@@ -1,25 +1,14 @@
+import { comparePromise, setTokens } from './jwt';
+
 import { GraphQLError } from 'graphql';
-import { compare } from 'bcrypt';
 import { findUserByUserName } from './users';
-import { setTokens } from './jwt';
 
-const comparePromise = async (password, hash) => {
-  return new Promise((resolve, reject) => {
-    compare(password, hash, (err, result) => {
-      if (err) reject(err);
-      else resolve(result);
-    });
-  });
-};
-
-export const login = async (_, { username, password }) => {
+export const login = async (_: any, { username, password }) => {
   const user = findUserByUserName(username);
   if (user && (await comparePromise(password, user.hash)))
     return setTokens(user);
   else
     throw new GraphQLError('Invalid credentials', {
-      extensions: {
-        code: 'UNAUTHENTICATED',
-      },
+      extensions: { code: 'UNAUTHENTICATED' },
     });
 };
