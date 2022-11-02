bd08860b4687a55920a8a3dddd83e37f511a0964
MichelFloyd <michel.floyd@cloaklabs.com>
2022-11-02 16:13:57 -0700

    remove unused stylesheet

--- reactNativeApp/App.tsx
+++ reactNativeApp/App.tsx
@@ -3,7 +3,6 @@ import { Navigation } from './navigation';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { client } from './graphql/client';
 
 export default function App() {
@@ -16,12 +15,3 @@ export default function App() {
     </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
